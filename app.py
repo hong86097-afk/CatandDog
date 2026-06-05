d5abc0695660d5ee09c98ec66194d6a7f05042e7
hong86097-afk <hong86097@gmail.com>
2026-06-05 08:34:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,13 +15,14 @@ import os
 
 @st.cache_resource
 def load_weights():
-    W1 = torch.tensor(np.load("W1.npy"), dtype=torch.float32)
-    b1 = torch.tensor(np.load("b1.npy"), dtype=torch.float32)
-    W2 = torch.tensor(np.load("W2.npy"), dtype=torch.float32)
-    b2 = torch.tensor(np.load("b2.npy"), dtype=torch.float32)
-    W3 = torch.tensor(np.load("W3.npy"), dtype=torch.float32)
-    b3 = torch.tensor(np.load("b3.npy"), dtype=torch.float32)
+    W1 = torch.tensor(np.load("animal/result/W1.npy"), dtype=torch.float32)
+    b1 = torch.tensor(np.load("animal/result/b1.npy"), dtype=torch.float32)
+    W2 = torch.tensor(np.load("animal/result/W2.npy"), dtype=torch.float32)
+    b2 = torch.tensor(np.load("animal/result/b2.npy"), dtype=torch.float32)
+    W3 = torch.tensor(np.load("animal/result/W3.npy"), dtype=torch.float32)
+    b3 = torch.tensor(np.load("animal/result/b3.npy"), dtype=torch.float32)
     return W1, b1, W2, b2, W3, b3
+
 
 
 
